49cd925bc6d839bf26b410e02f302492710bc072
Kanteera Bunrueang <kanteera-b@rmutp.ac.th>
2026-09-24 17:40:11 +0700

    เพิ่ม ข้อ3

--- strapi-custom/config/admin.js
+++ strapi-custom/config/admin.js
@@ -2,9 +2,6 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
   },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
   rateLimit: {
     enabled: true,
     interval: 60000,
